54a9757a9b68fb876092e9f722b5e5279ea7c18d
Daneruname <63127831+Daneruname@users.noreply.github.com>
2021-03-08 15:18:44 -0300

    Add files via upload

---
+++ Atividade3/Atividade3/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Atividade3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(42, 51);
+            this.lblA.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(62, 19);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Lado A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(44, 102);
+            this.lblB.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(60, 19);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "Lado B";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(44, 155);
+            this.lblC.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(61, 19);
+            this.lblC.TabIndex = 2;
+            this.lblC.Text = "Lado C";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(136, 48);
+            this.txtA.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(164, 26);
+            this.txtA.TabIndex = 3;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(136, 99);
+            this.txtB.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(164, 26);
+            this.txtB.TabIndex = 4;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(136, 152);
+            this.txtC.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(164, 26);
+            this.txtC.TabIndex = 5;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(31, 217);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(90, 77);
+            this.btnLimpar.TabIndex = 6;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(152, 217);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(90, 77);
+            this.btnSair.TabIndex = 7;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(278, 217);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(90, 77);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(402, 339);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Font = new System.Drawing.Font("NanumGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Atividade3/Atividade3/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade3
+{
+    public partial class Form1 : Form
+    {
+        double ladoA, ladoB, ladoC;
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtA.Clear();
+            txtB.Clear();
+            txtC.Clear();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtA.Text, out ladoA) &&
+                double.TryParse(txtB.Text, out ladoB) &&
+                double.TryParse(txtC.Text, out ladoC))
+
+                
+            {
+                if (Math.Sqrt((ladoB - ladoC) * (ladoB - ladoC)) < ladoA && ladoA < ladoB + ladoC &&
+                   (Math.Sqrt((ladoA - ladoC) * (ladoA - ladoC)) < ladoB && ladoB < ladoA + ladoC &&
+                   (Math.Sqrt((ladoA - ladoB) * (ladoA - ladoB)) < ladoC && ladoC < ladoA + ladoB)))
+                {
+                    if ((ladoA == ladoB) && (ladoC == ladoB))
+                    {
+                        MessageBox.Show("Equilátero");
+                    }
+
+                    else
+
+                    {
+
+                        if ((ladoA != ladoB) && (ladoB != ladoC) && (ladoC != ladoA))
+                        {
+                            MessageBox.Show("Escaleno");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Isósceles");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
